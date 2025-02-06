6040311e167dfa4b06bcf4684818093e50557af5
essie20 <ryghl683@gmail.com>
2025-02-06 12:55:48 +0200

    use port from env

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
